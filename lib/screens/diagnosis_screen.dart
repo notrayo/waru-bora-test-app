a54eb9a7d390fe3f5e8fe578fafb704ee389af0b
notrayo <rayoleeroy@gmail.com>
2023-04-12 20:11:56 +0300

    added image picker dependancy, upgraded android SDK version and added a pick image button

--- lib/screens/diagnosis_screen.dart
+++ lib/screens/diagnosis_screen.dart
@@ -2,17 +2,35 @@ import 'package:flutter/material.dart';
 //import 'package:flutter/src/widgets/container.dart';
 //import 'package:flutter/src/widgets/framework.dart';
 
-class DiagnosisScreen extends StatelessWidget {
+class DiagnosisScreen extends StatefulWidget {
   const DiagnosisScreen({super.key});
 
   @override
+  State<DiagnosisScreen> createState() => _DiagnosisScreenState();
+}
+
+class _DiagnosisScreenState extends State<DiagnosisScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       //app bar
       //body
       body: Center(
-        child: Text('Photo of sick plant will be uploaded here : '),
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blueAccent),
+            child: const Text('upload photo of sick plant'),
+          ),
+          const SizedBox(
+            height: 10,
+          )
+        ],
+      )),
     );
   }
 }
