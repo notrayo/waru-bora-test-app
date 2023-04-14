3db3141e5391b5570befd8858a4907fadeae6dba
notrayo <rayoleeroy@gmail.com>
2023-04-14 13:04:05 +0300

    added a predict button

--- lib/screens/diagnosis_screen.dart
+++ lib/screens/diagnosis_screen.dart
@@ -38,9 +38,13 @@ class _DiagnosisScreenState extends State<DiagnosisScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           _imageFile != null
-              ? Image.file(_imageFile!, height: 150)
+              ? Image.file(
+                  _imageFile!,
+                  height: 300,
+                  width: 300,
+                )
               : const SizedBox(
-                  height: 150,
+                  height: 100,
                   child: Center(
                     child: Text('No image selected'),
                   ),
@@ -68,7 +72,25 @@ class _DiagnosisScreenState extends State<DiagnosisScreen> {
                 child: const Text('Select from gallery'),
               ),
             ],
-          )
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Divider(
+            height: 1,
+            thickness: 1,
+            color: Colors.grey[300],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          //prediction button
+          ElevatedButton(
+              onPressed: (() {}),
+              style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.redAccent,
+                  backgroundColor: const Color.fromARGB(255, 239, 234, 234)),
+              child: const Text('PREDICTION'))
         ],
       )),
     );
