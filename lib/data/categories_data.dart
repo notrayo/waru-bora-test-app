a1688a15ef968d7a175a6cf95df87e1d441675cb
notrayo <rayoleeroy@gmail.com>
2023-04-17 16:29:57 +0300

    created categories for medication

---
+++ lib/data/categories_data.dart
@@ -0,0 +1,22 @@
+//import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../models/disease_category.dart';
+
+const dummyCategories = [
+  DiseaseCategory(
+    id: 'c1',
+    title: 'Early Blight',
+    color: Color.fromARGB(255, 53, 6, 239),
+  ),
+  DiseaseCategory(
+    id: 'c2',
+    title: 'Late Blight',
+    color: Color.fromARGB(255, 180, 81, 45),
+  ),
+  DiseaseCategory(
+    id: 'c3',
+    title: 'Healthy Plant',
+    color: Color.fromARGB(255, 81, 153, 118),
+  )
+];
--- lib/screens/treatment_screen.dart
+++ lib/screens/treatment_screen.dart
@@ -2,13 +2,85 @@ import 'package:flutter/material.dart';
 //import 'package:flutter/src/widgets/container.dart';
 //import 'package:flutter/src/widgets/framework.dart';
 
+import '../models/disease_category.dart';
+import '../data/categories_data.dart';
+
+class CategoryItem extends StatelessWidget {
+  final DiseaseCategory category;
+
+  const CategoryItem({Key? key, required this.category}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(15),
+        child: Container(
+          height: 120,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+              colors: [
+                category.color.withOpacity(0.7),
+                category.color,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                category.title,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class TreatmentScreen extends StatelessWidget {
   const TreatmentScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Treatment options / items will be here : ')),
+    return Scaffold(
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(12),
+            child: Text(
+              'Please pick the diagnosis you got of the potato plant so as to view viable medication:',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: dummyCategories.length,
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.all(15),
+                child: CategoryItem(category: dummyCategories[index]),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
