c3e7e123d43c579ba9373c33f4b85a6c5ddb38c0
notrayo <rayoleeroy@gmail.com>
2023-04-27 12:53:41 +0300

    added content to the details page

--- lib/screens/madawa_details_screen.dart
+++ lib/screens/madawa_details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:waru_bora_test/data/medicine_data.dart';
 
 class DawaDetailsScreen extends StatelessWidget {
   static const routeName = '/dawa-detail';
@@ -8,11 +9,106 @@ class DawaDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dawaID = ModalRoute.of(context)?.settings.arguments as String;
+
+    final selectedMedicine =
+        // ignore: non_constant_identifier_names
+        dummyMedicine.firstWhere((Dawa) => Dawa.id == dawaID);
+
+    double listViewHeight = selectedMedicine.benefits.length * 60.0 + 10.0;
+
+    double listViewHeight2 = selectedMedicine.howToUse.length * 60.0 + 10.0;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(dawaID),
-      ),
-      body: Text('medicine details of $dawaID'),
-    );
+        appBar: AppBar(
+          title: Text(selectedMedicine.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: Image.asset(
+                  selectedMedicine.imageLink,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 15),
+                child: const Text(
+                  'Benefits',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 123, 84, 70),
+                    //decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10)),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                height: listViewHeight,
+                width: 350,
+                child: ListView.builder(
+                    itemBuilder: (context, index) => Card(
+                          color: const Color.fromARGB(255, 136, 184, 206),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Text(selectedMedicine.benefits[index],
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontStyle: FontStyle.italic)),
+                          ),
+                        ),
+                    itemCount: selectedMedicine.benefits.length),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(vertical: 15),
+                child: const Text(
+                  'How To Use',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 83, 204, 12),
+                    //decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white10,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10)),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                height: listViewHeight2,
+                width: 350,
+                child: ListView.builder(
+                    itemBuilder: (context, index) => Card(
+                          color: const Color.fromARGB(255, 92, 142, 99),
+                          child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Text(selectedMedicine.howToUse[index],
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontStyle: FontStyle.italic)),
+                          ),
+                        ),
+                    itemCount: selectedMedicine.howToUse.length),
+              ),
+            ],
+          ),
+        ));
   }
 }
