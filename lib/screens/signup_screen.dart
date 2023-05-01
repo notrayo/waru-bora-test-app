608b7ff5c8c034b0c731a43c757412dde8e95e1d
notrayo <rayoleeroy@gmail.com>
2023-05-01 21:14:31 +0300

    updated signup UI

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -14,7 +14,106 @@ class _SignUpScreenState extends State<SignUpScreen> {
       appBar: AppBar(
         title: const Text('SIGN UP SCREEN'),
       ),
-      body: Container(),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromARGB(255, 255, 255, 255),
+            Color.fromARGB(255, 255, 255, 255),
+          ],
+        )),
+        child: SingleChildScrollView(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(
+                  20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'First Name : ',
+                      prefixIcon: const Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(
+                            color: Color.fromARGB(255, 2, 63, 113)),
+                      ),
+                      hintStyle: const TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Last Name : ',
+                      prefixIcon: const Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(
+                            color: Color.fromARGB(255, 2, 63, 113)),
+                      ),
+                      hintStyle: const TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter your Email : ',
+                      prefixIcon: const Icon(Icons.email),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: const BorderSide(
+                            color: Color.fromARGB(255, 2, 63, 113)),
+                      ),
+                      hintStyle: const TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: 'Enter desired Password',
+                        prefixIcon: const Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        hintStyle: const TextStyle(color: Colors.black)),
+                  ),
+                  const SizedBox(height: 30),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: 'Re-Enter Password...',
+                        prefixIcon: const Icon(Icons.lock),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        hintStyle: const TextStyle(color: Colors.black)),
+                  ),
+                  const SizedBox(height: 30),
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/login');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 80,
+                        vertical: 15,
+                      ),
+                    ),
+                    child: const Text('Submit'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
